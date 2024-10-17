b41eacdaed57633895625cd99846f4cf36a76d52
xser56 <gettosheep56@gmail.com>
2024-10-17 10:40:50 -0700

    Test 1

--- obj/Debug/net8.0/GroupCYOA.AssemblyInfo.cs
+++ obj/Debug/net8.0/GroupCYOA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd3089d347b225632317fc98f3b6044a2c84d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d05592e40f0816600913cd167659e15b0da11b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
