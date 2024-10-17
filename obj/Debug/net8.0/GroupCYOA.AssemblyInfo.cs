a108cdffed942d9192ea408d65bdb454950de307
wraithio <aarmon5@gmail.com>
2024-10-17 14:32:19 -0700

    merge

--- obj/Debug/net8.0/GroupCYOA.AssemblyInfo.cs
+++ obj/Debug/net8.0/GroupCYOA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d793c3b547fbdc9d02bd0c2885a2c1971480b8a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9583bd5ad40f1e88278dd9fe82e8a77e6ac012")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupCYOA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
